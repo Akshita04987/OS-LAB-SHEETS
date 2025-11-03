fa5b4115cf77e5637e49aafcfd39ed6cac8a9ccd
Akshita04987 <143160146+Akshita04987@users.noreply.github.com>
2025-11-03 11:48:15 +0530

    concurrent process creation
    Added logging and time delay to system_process function.

--- LAB SHEET 2/subtask3.py
+++ LAB SHEET 2/subtask3.py
@@ -1,13 +1,20 @@
 import multiprocessing
-from subtask2 import system_process
+import logging
+import time
+
+# Dummy function to simulate a task
+def system_process(task_name):
+    logging.info(f"{task_name} started")
+    time.sleep(2)
+    logging.info(f"{task_name} ended")
+
 if __name__ == '__main__':
-	print("System Starting...")
-	p1 = multiprocessing.Process(target=system_process, args=('Process-1',))
-	p2 = multiprocessing.Process(target=system_process, args=('Process-2',))
-	p1.start()
-	p2.start()
-	
-    # Wait for processes to complete
-	p1.join()
-	p2.join()
-	print("System Shutdown.")
+    print("System Starting...")
+
+    # Create processes
+    p1 = multiprocessing.Process(target=system_process, args=('Process-1',))
+    p2 = multiprocessing.Process(target=system_process, args=('Process-2',))
+
+    # Start processes
+    p1.start()
+    p2.start()
