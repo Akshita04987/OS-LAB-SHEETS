34c2fac3837a51d7c8ac4c833cb10a42c4a52819
Akshita04987 <143160146+Akshita04987@users.noreply.github.com>
2025-11-03 11:44:58 +0530

    process simulation function

--- LAB SHEET 2/subtask2.py
+++ LAB SHEET 2/subtask2.py
@@ -1,6 +1,7 @@
-import logging 
 import time
+import logging
+# Dummy function to simulate a process task
 def system_process(task_name):
-	logging.info(f"{task_name} started")
-	time.sleep(2)  # Simulate task delay
-	logging.info(f"{task_name} ended")
+    logging.info(f"{task_name} started")
+    time.sleep(2)  # Simulate some work
+    logging.info(f"{task_name} ended")
